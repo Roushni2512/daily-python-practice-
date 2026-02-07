49097c4735885b686d7ecdd80c6502c26ccfa354
roushni zynab <zynabroushni@gmail.com>
2026-02-07 22:15:30 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Addition:", add(x, y))
+print("Subtraction:", sub(x, y))
